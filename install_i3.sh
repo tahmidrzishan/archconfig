75592aec37425de4019c9f539b99e9313935452d
Tahmid Zishan <tahmidzishan53@gmail.com>
2025-06-20 08:44:46 +0600

    Minor fix

--- install_i3.sh
+++ install_i3.sh
@@ -1,4 +1,12 @@
-sudo pacman -S --needed brightnessctl i3 i3status alacritty dmenu feh rofi xorg-xinit xorg-server
+# Ensure script is run as root
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run this script as root (use sudo)"
+  exit 1
+fi
+
+
+pacman -S --needed brightnessctl i3 i3status alacritty dmenu feh rofi xorg-xinit xorg-server
+mkdir -p ~/.config/i3/
 cp i3_config ~/.config/i3/config
 echo "Installed i3 config file in ~/.config/i3/config"
 echo "i3 Window Manager will automatically detect the config file"
@@ -9,12 +17,6 @@ echo "i3 Window Manager will automatically detect the config file"
 
 CONFIG_DIR="/etc/X11/xorg.conf.d"
 CONFIG_FILE="$CONFIG_DIR/40-libinput.conf"
-
-# Ensure script is run as root
-if [[ $EUID -ne 0 ]]; then
-  echo "Please run this script as root (use sudo)"
-  exit 1
-fi
 
 # Create config directory if needed
 mkdir -p "$CONFIG_DIR"
